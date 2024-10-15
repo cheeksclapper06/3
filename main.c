7bb23438223eead4a9b7c7a9b73bd1df5024bc37
kenny <badnight0903@gmail.com>
2024-10-15 22:46:33 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <math.h>
-#include <conio.h>
+#include "root_calculations.h"
 #include "functions.h"
 
 //всем сканф написать валидацию для ввода символов, чтобы выводилась ошибка и цикл повторялся
 int main()
 {
-    double x = 0, e = 0;
+    double x = 0, e = 0, y = 0;
     int k = 0;
 
     do
@@ -15,47 +14,41 @@ int main()
 
         k = get_root_value();
 
-        //PROBLEM SOS SOS SOS написать нормальную валидацию для икса
-            printf("Enter the root expression (x)\n");
-            scanf("%lf", &x);
-            fflush(stdin);
-
+        x = get_root_base(k);
         e = get_accuracy();
 
-        double delta = 1.0, y = 1.0;
-        int i = 1;
-        while (fabs(delta) >= e)
-        {
-            delta = (1.0 / k) * (x / (int)pow(y, k - 1) - y);
-
-            if (fabs(delta) < e)
-            {
-                break;
-            }
-
-            y += delta;
-            i++;
-        }
+        y = calculate_root(k, x, e);
 
         char output_form = 0;
-        printf("Choose the output form: standard (1) or exponential (2):\n");
-        scanf(" %c", &output_form);
-        fflush(stdin);
-        //пофиксить проблему с getchar
-        if (output_form == '1')
+        do
         {
-            int n = 0;
-            n = get_decimal_places();
-            printf("The calculated Y is: %.*lf\n", n, y);
+            printf("Choose the output form: standard (1) or exponential (2):\n");
+            scanf(" %c", &output_form);
+            fflush(stdin);
+            //пофиксить проблему с getchar
+            switch (output_form)
+            {
+                case '1':
+                    int n = 0;
+                n = get_decimal_places();
+                printf("The calculated Y is: %.*lf\n", n, y);
+                break;
+
+                case '2':
+                    printf("The calculated Y is: %e\n", y);
+                break;
+
+                default:
+                    printf("You can choose only between number 1 or 2\n");
+                break;
+            }
         }
-        else if (output_form == '2')
-        {
-            printf("The calculated Y is: %e\n", y);
-        }
+        while (output_form < '1' && output_form > '2');
         printf ("Do you want to restart?\n");
         printf("If yes press ENTER, otherwise press any key to close the program\n");
     }
     while (getchar() != 13);
     fflush(stdin);
     return 0;
+
 }
