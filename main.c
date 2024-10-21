2e38228ac6b34cd0b354ab709d1309f9885a563f
kenny <badnight0903@gmail.com>
2024-10-21 14:06:31 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+#include <conio.h>
 #include "root_calculations.h"
 #include "functions.h"
 
-//всем сканф написать валидацию для ввода символов, чтобы выводилась ошибка и цикл повторялся
 int main()
 {
-    double x = 0, e = 0, y = 0;
+    double x = 0, e = 0;
     int k = 0;
 
     do
@@ -13,41 +13,39 @@ int main()
         printf("This application will calculate the K-th root of an X with specified accuracy (E)\n");
 
         k = get_root_value();
-
         x = get_root_base(k);
         e = get_accuracy();
 
-        y = calculate_root(k, x, e);
+        const double y = calculate_root(k, x, e);
 
-        char output_form = 0;
+        char output = 0;
         do
         {
             printf("Choose the output form: standard (1) or exponential (2):\n");
-            scanf(" %c", &output_form);
-            fflush(stdin);
-            //пофиксить проблему с getchar
-            switch (output_form)
+            output = getchar();
+            while (getchar() != '\n');
+            switch (output)
             {
-                case '1':
+                case 49:
                     int n = 0;
                 n = get_decimal_places();
                 printf("The calculated Y is: %.*lf\n", n, y);
-                break;
+                continue;
 
-                case '2':
+                case 50:
                     printf("The calculated Y is: %e\n", y);
-                break;
+                continue;
 
                 default:
                     printf("You can choose only between number 1 or 2\n");
                 break;
             }
         }
-        while (output_form < '1' && output_form > '2');
+        while (output < 49 || output > 51);
         printf ("Do you want to restart?\n");
         printf("If yes press ENTER, otherwise press any key to close the program\n");
     }
-    while (getchar() != 13);
+    while (getch() == 13);
     fflush(stdin);
     return 0;
 
